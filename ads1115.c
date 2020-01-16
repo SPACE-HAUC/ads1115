1eae5952bb293e5856883d808e4a3280c703a52a
Nicholas Sacco <nicksacco17@gmail.com>
2020-01-16 16:18:01 -0500

    ADS1115 V0.A

---
+++ ads1115.c
@@ -0,0 +1,125 @@
+
+#include "ads1115.h"
+
+int ads1115_init(ads1115 *dev, uint8_t s_address)
+{
+    dev->fd = open(I2C_BUS, O_RDWR);
+    if (dev->fd < 0)
+    {
+        perror("[ERROR] Could not create a file descriptor.");
+        return -1;
+    }
+
+    if (ioctl(dev->fd, I2C_SLAVE, s_address) < 0)
+    {
+        perror("[ERROR] Could not assign device.");
+        return -1;
+    }
+
+    return 1;
+}
+
+int ads1115_configure(ads1115 *dev, ads1115_config m_con)
+{
+    uint8_t buf[3];
+    buf[0] = CONFIG_REG;
+    buf[1] = m_con.raw >> 8;
+    buf[2] = m_con.raw & 0xFF;
+
+    if (write(dev->fd, buf, 3) < 3)
+    {
+        perror("[ERROR] Could not configure device.");
+        return -1;
+    }
+    return 1;
+}
+
+int ads1115_read(ads1115 *dev, short *data)
+{
+    uint8_t buf[3], status = 1;
+
+    ads1115_config m_con;
+    m_con.raw = 0;
+    m_con.os = 1;
+    m_con.mux = 0x04;
+    m_con.pga = 1;
+    m_con.mode = 1;
+    m_con.dr = 6;
+
+    do
+    {
+        buf[0] = CONFIG_REG;
+        buf[1] = m_con.raw >> 8;
+        buf[2] = m_con.raw & 0xFF;
+
+        if (write(dev->fd, buf, 3) < 3)
+        {
+            perror("[ERROR] Could not access target register.");
+            return -1;
+        }
+
+        if (write(dev->fd, buf, 1) < 1)
+        {
+            perror("Could not stat read");
+        }
+
+        if (read(dev->fd, &buf[1], 2) < 2)
+        {
+            perror("Could not stat read command");
+        }
+        m_con.raw = (((uint16_t)buf[1]) << 8) | buf[2];
+    } while (m_con.os > 0);
+
+    usleep(1500);
+
+    do
+    {
+        buf[0] = CONFIG_REG;
+        if (write(dev->fd, buf, 1) < 1)
+        {
+            perror("Could not stat read");
+        }
+
+        if (read(dev->fd, &buf[1], 2) < 2)
+        {
+            perror("Could not stat read command");
+        }
+        m_con.raw = (((uint16_t)buf[1]) << 8) | buf[2];
+    } while (m_con.os == 0);
+
+    m_con.raw = 0;
+    m_con.os = 1;
+    m_con.mux = 0x04;
+    m_con.pga = 1;
+    m_con.mode = 1;
+    m_con.dr = 6;
+    for (int i = 0; i < 4; i++)
+    {
+        buf[0] = CONVERSION_REG;
+        buf[1] = 0x00;
+        buf[2] = 0x00;
+        if (write(dev->fd, buf, 1) < 1)
+        {
+            perror("Could not stat read");
+            status = 0;
+        }
+
+        if (read(dev->fd, &buf[1], 2) < 2)
+        {
+            perror("Could not stat read command");
+            status = 0;
+        }
+        data[i] = (((uint16_t)buf[1]) << 8) | buf[2];
+        buf[0] = CONFIG_REG;
+        m_con.mux++;
+        buf[1] = m_con.raw >> 8;
+        
+        buf[2] = m_con.raw & 0xff;
+        if (write(dev->fd, buf, 3) < 3)
+        {
+            perror("Could not stat mux change");
+            status = 0;
+        }
+    }
+    return status;
+}
---
+++ ads1115.h
@@ -0,0 +1,67 @@
+/*
+ *******************************************************************************
+ * Title: ads1115.h
+ * Description: Device driver (I2C) for ADS1115 ADC used with Fine Sun Sensor
+ * Update:
+ *          0 - Module created [2020/16/1] [NS]
+ * Status - REVIEW REQUIRED
+ ******************************************************************************* 
+ */
+
+#ifndef ADS1115_H
+#define ADS1115_H
+
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/i2c-dev.h>
+#include <signal.h>
+
+/* I2C ADDRESSING */
+#define ADS1115_S_ADDR 0x48             // Device address for ADS1115
+#define I2C_BUS "/dev/i2c-0"            // I2C bus name
+
+/* REGISTERS */
+#define CONVERSION_REG 0x00            // ADC conversion register
+#define CONFIG_REG 0x01                 // ADC configuration register
+#define CONFIG_REG_OS 0x80
+#define CONFIG_REG_MUX_0 0x40
+#define CONFIG_REG_MUX_1 0x50
+#define CONFIG_REG_MUX_2 0x60
+#define CONFIG_REG_MUX_3 0x70
+
+typedef union
+{
+    struct
+    {
+        uint8_t comp_que : 2;
+        uint8_t comp_lat : 1;
+        uint8_t comp_mode : 1;
+        uint8_t comp_pol : 1;
+        uint8_t dr : 3;
+        uint8_t mode : 1;
+        uint8_t pga : 3;
+        uint8_t mux : 3;
+        uint8_t os : 1;
+    };
+    uint16_t raw;
+} ads1115_config;
+
+/* ADS1115 DATA STRUCTURE */
+typedef struct
+{
+    int fd;
+    char fname[40];
+} ads1115;
+
+int ads1115_init(ads1115* dev, uint8_t s_address);
+int ads1115_configure(ads1115* dev, ads1115_config m_con);
+int ads1115_read(ads1115 *dev, short *data);
+
+#endif // ADS1115_H
